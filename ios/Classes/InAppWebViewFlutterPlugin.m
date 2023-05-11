d0abf4c041cd5fc861f1f8171ccc1fc4090769c8
Paolo Tolentino <paolo.tolentino@enstack.com>
2023-05-11 18:25:00 +0800

    fixed iOS swift class import (2)

--- ios/Classes/InAppWebViewFlutterPlugin.m
+++ ios/Classes/InAppWebViewFlutterPlugin.m
@@ -22,7 +22,7 @@
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_inappwebview_fix_iostouchdelay-Swift.h"
+#import "flutter_inappwebview-Swift.h"
 #endif
 
 @implementation InAppWebViewFlutterPlugin : NSObject
